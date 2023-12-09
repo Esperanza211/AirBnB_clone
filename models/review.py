d07066c3df35be2ec7440f499b3ffa67170eb742
imanebenhadda <imanebenhaddapama@gmail.com>
2023-12-09 12:57:25 +0100

    add

---
+++ models/review.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""importing BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """a review class that inherits from BaseModel"""
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    
